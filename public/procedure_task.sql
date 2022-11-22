f39b98dce487e815156a28b96e2a96e74b6c6e0a
Karpuhin Ilya <karpuhinilya@gmail.com>
2022-11-22 13:15:18 +0300

    Procedure one

--- public/procedure_task.sql
+++ public/procedure_task.sql
@@ -1,53 +1,19 @@
-/*
- procedure
- get the table name as input, return the number of rows in it
- */
+CREATE OR REPLACE FUNCTION tripAfterDate (after timestamp without time zone)
+RETURNS TABLE(id integer, plane character varying, town_from character varying, town_out character varying, time_out timestamp)
+LANGUAGE 'plpgsql'
+COST 100
+VOLATILE PARALLEL UNSAFE
+ROWS 1000
 
-/*
- create or replace function pr1 (tbl regclass) returns integer as $$
-begin
-    set result = (select count(*) from tbl);
+AS $BODY$ BEGIN
+    return query (
+        select * from trip
+                 where trip.time_out > after
+    );
 end;
-$$ language sql;
- */
+$BODY$;
 
+ALTER FUNCTION tripAfterDate(after timestamp without time zone)
+    OWNER TO postgres;
 
-/* ------------------------------------- */
-create function pr1 (tablename text) returns integer
-as
-$body$
-declare
-    result integer;
-BEGIN
-    execute tablename into result;
-    return result;
-end;
-$body$
-    language plpgsql;
-
-
-/* ------------------------------------- */
-create function
-    cnt_rows(schema text, tablename text) returns integer
-as
-$body$
-declare
-    result integer;
-    query varchar;
-begin
-    query := 'SELECT count(1) FROM ' || schema || '.' || tablename;
-    execute query into result;
-    return result;
-end;
-$body$
-    language plpgsql;
-
-/* ------------------------------------- */
-CREATE OR REPLACE FUNCTION some_f(_tbl regclass, OUT result integer)
-    LANGUAGE plpgsql AS
-$func$
-BEGIN
-    EXECUTE format('SELECT (EXISTS (SELECT FROM %s WHERE id = 1))::int', _tbl)
-        INTO result;
-END
-$func$;
+----------------------------------------------------------------------------------------------
