1500db385bd2cd2a84cb402ba2823ae6e65a53ee
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-01-20 14:26:52 +0300

    update Server and add Client Client can be get information from server

---
+++ src/ClientServerSocket/Client.java
@@ -0,0 +1,27 @@
+package ClientServerSocket;
+
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException{
+        Socket clientSocket = new Socket("127.0.0.1", 8000);
+
+        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        writer.write("HTTP/1.0 200 OK\r\n" +
+                "Content-type: text/html\r\n" +
+                "\r\n" +
+                "<h1>Get me some information<h1>\n");
+        writer.flush();
+
+
+        String message = reader.readLine();
+        System.out.println(message);
+
+        writer.close();
+        reader.close();
+        clientSocket.close();
+    }
+}
--- src/ClientServerSocket/Server.java
+++ src/ClientServerSocket/Server.java
@@ -14,13 +14,15 @@ public class Server {
 
         while(true) {
             Socket clientSocket = serverSocket.accept();
-            System.out.println("Client accepted " + (count++));
+            System.out.println("Client accepted " + (++count));
 
             OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
-            writer.write("<h1>Java</h1>");
+            writer.write("HTTP/1.0 200 OK\r\n" +
+                    "Content-type: text/html\r\n" +
+                    "\r\n" +
+                    "<h1>You are client #" + count + "<h1>\n");
             writer.flush();
             writer.close();
-
             clientSocket.close();
         }
 
