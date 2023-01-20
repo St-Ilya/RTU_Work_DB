9172dec9f283bb9db216a03050eb928c6817178e
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-01-20 13:47:17 +0300

    update Server You can call the server and get an auto answer, a connection counter has also been added

--- src/ClientServerSocket/Server.java
+++ src/ClientServerSocket/Server.java
@@ -1,19 +1,30 @@
 package ClientServerSocket;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
     public static void main(String[] args) throws IOException{
-        Server.ServerSoc();
-    }
 
-    public static void ServerSoc() throws IOException {
+        int count = 0;
         ServerSocket serverSocket = new ServerSocket(8000);
 
-        serverSocket.accept();
+        while(true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client accepted " + (count++));
 
-        serverSocket.close();
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            writer.write("<h1>Java</h1>");
+            writer.flush();
+            writer.close();
+
+            clientSocket.close();
+        }
+
+
+        //serverSocket.close();
     }
 }
