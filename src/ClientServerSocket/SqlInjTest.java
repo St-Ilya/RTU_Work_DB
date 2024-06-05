4dc55d2e2a081d61a5c52ec49c1248a17913b2a3
St-Ilya <karpuhinilya@gmail.com>
2024-06-05 13:49:02 +0300

    add second test sql inj

--- src/ClientServerSocket/SqlInjTest.java
+++ src/ClientServerSocket/SqlInjTest.java
@@ -11,22 +11,21 @@ import javax.management.relation.Role;
 
 public class SqlInjTest {
     public static List<Employee> func(String name) {
-String sql = "select * from employee where name ='" + name + "'";
-Statement dataSource;
-try (Connection c = dataSource.getConnection();
-ResultSet rs = c.createStatement().executeQuery(sql)) {
-    List<Employee> employees = new ArrayList<>();
-    while (rs.next()) {
-        employees.add(new Employee(
-            rs.getLong("id"),
-            rs.getString("name"),
-            rs.getString("password"),
-            Role.valueOf(rs.getString("role"))
-            ));
+        String sql = "select * from employee where name ='" + name + "'";
+        Statement dataSource;
+        try (Connection c = dataSource.getConnection();
+                ResultSet rs = c.createStatement().executeQuery(sql)) {
+            List<Employee> employees = new ArrayList<>();
+            while (rs.next()) {
+                employees.add(new Employee(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getString("password"),
+                        Role.valueOf(rs.getString("role"))));
+            }
+            return employees;
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
         }
-        return employees;
-    } catch (SQLException ex) {
-        throw new RuntimeException(ex);
     }
-}   
-    }
+}
---
+++ src/ClientServerSocket/SqlnjTestTwo
@@ -0,0 +1,32 @@
+package ClientServerSocket;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.relation.Role;
+
+public class SqlInjTest {
+    public static List<Employee> func(String name) {
+String sql = "select * from employee where name ='" + name + "'";
+Statement dataSource;
+try (Connection c = dataSource.getConnection();
+ResultSet rs = c.createStatement().executeQuery(sql)) {
+    List<Employee> employees = new ArrayList<>();
+    while (rs.next()) {
+        employees.add(new Employee(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getString("password"),
+            Role.valueOf(rs.getString("role"))
+            ));
+        }
+        return employees;
+    } catch (SQLException ex) {
+        throw new RuntimeException(ex);
+    }
+}   
+    }
